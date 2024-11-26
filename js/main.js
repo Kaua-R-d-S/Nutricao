64f717aa26fd83d390c0ca1abd9b54f94ec8e8a5
Kauã <126888652+Kaua-R-d-S@users.noreply.github.com>
2024-11-26 00:32:15 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "NutriRodri";
+var titulo = document.querySelector("h2");
+titulo.textContent = "Pacientes Cadastrados";
+
+
+var pacientes = document.querySelectorAll("#primeiro-paciente");
+console.log(paciente);
